a33cb53a0d5ba7ffc68ed2d7354f4813c9b85950
google85 <bpfcomp2005@gmail.com>
2026-02-08 02:24:30 +0200

    refactor with asciiz message for characters to be printed

--- src/lcd-print.s
+++ src/lcd-print.s
@@ -28,35 +28,18 @@ reset:
     lda #%00000001  ; Clear display
     jsr lcd_instruction
 
-    lda #"H"
+    ldx #0
+print:
+    lda message,x
+    beq loop        ; Exit this loop when '\0' encountered
     jsr print_char
-    lda #"e" 
-    jsr print_char
-    lda #"l"
-    jsr print_char
-    lda #"l"
-    jsr print_char
-    lda #"o"
-    jsr print_char
-    lda #","
-    jsr print_char
-    lda #" "
-    jsr print_char
-    lda #"w"
-    jsr print_char
-    lda #"o"
-    jsr print_char
-    lda #"r"
-    jsr print_char
-    lda #"l"
-    jsr print_char
-    lda #"d"
-    jsr print_char
-    lda #"!"
-    jsr print_char
-
+    inx
+    jmp print
+    
 loop:
     jmp loop
+
+message: .asciiz "   Hello, world!  "
 
 lcd_wait:
     pha             ; push A value to stack
